234639c9c14efaa1ac557a73eb6b730a61faf21b
yveshield <yveshield@gmail.com>
2020-03-20 23:17:52 +0800

    137. Single Number II

---
+++ 137.go
@@ -0,0 +1,23 @@
+package leetcode
+
+func singleNumber(nums []int) int {
+	p := make(map[int]int)
+	p[nums[0]] = 1
+	for i := 1; i < len(nums); i++ {
+		if k, ok := p[nums[i]]; ok {
+			if k == 2 {
+				delete(p, nums[i])
+			} else {
+				p[nums[i]]++
+			}
+		} else {
+			p[nums[i]] = 1
+		}
+	}
+	var q int
+	for k := range p {
+		q = k
+	}
+
+	return q
+}
