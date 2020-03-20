14a52b34992f7cf431597ddcbbd420ef3e6d5e5f
yveshield <yveshield@gmail.com>
2020-03-20 23:10:54 +0800

    503. Next Greater Element II

---
+++ 503.go
@@ -0,0 +1,26 @@
+package leetcode
+
+func nextGreaterElements(nums []int) []int {
+	p := make([]int, len(nums))
+	for i := 0; i < len(nums); i++ {
+		found := false
+		p[i] = -1
+		for j := i + 1; j < len(nums); j++ {
+			if nums[j] > nums[i] {
+				p[i] = nums[j]
+				found = true
+				break
+			}
+		}
+		if !found {
+			for k := 0; k < i; k++ {
+				if nums[k] > nums[i] {
+					p[i] = nums[k]
+					break
+				}
+			}
+		}
+
+	}
+	return p
+}
