2d5aa0cab13913af91b487443fb9372fb43adcf1
yveshield <yveshield@gmail.com>
2020-03-21 00:07:09 +0800

    334. Increasing Triplet Subsequence

---
+++ 334.go
@@ -0,0 +1,18 @@
+package leetcode
+
+const MaxInt = int(^uint(0) >> 1)
+
+func increasingTriplet(nums []int) bool {
+	m1 := MaxInt
+	m2 := MaxInt
+	for i := 0; i < len(nums); i++ {
+		if nums[i] <= m1 {
+			m1 = nums[i]
+		} else if nums[i] <= m2 {
+			m2 = nums[i]
+		} else {
+			return true
+		}
+	}
+	return false
+}
