8fc4fd1e75b19ad35010e2f37d69a56989a8bc5b
yveshield <yveshield@gmail.com>
2020-03-20 23:28:31 +0800

    2. Add Two Numbers

---
+++ 2.go
@@ -0,0 +1,32 @@
+package leetcode
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	l3 := &ListNode{}
+	j := l3
+	up := 0
+	for l1 != nil || l2 != nil || up > 0 {
+		if l1 != nil {
+			up += l1.Val
+			l1 = l1.Next
+		}
+		if l2 != nil {
+			up += l2.Val
+			l2 = l2.Next
+		}
+		l3.Val = up % 10
+		up /= 10
+		if l1 != nil || l2 != nil || up > 0 {
+			l3.Next = &ListNode{}
+			l3 = l3.Next
+		}
+	}
+	return j
+
+}
