6cd99542d95a9bad003fe972ae506e5a1bc2102f
yveshield <yveshield@gmail.com>
2020-03-20 23:16:20 +0800

    240. Search a 2D Matrix II

---
+++ 240.go
@@ -0,0 +1,17 @@
+package leetcode
+
+func searchMatrix(matrix [][]int, target int) bool {
+	row := len(matrix) - 1
+	col := 0
+
+	for row >= 0 && col < len(matrix[0]) {
+		if matrix[row][col] > target {
+			row--
+		} else if matrix[row][col] < target {
+			col++
+		} else {
+			return true
+		}
+	}
+	return false
+}
