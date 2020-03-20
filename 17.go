487f6323a831e9d04a21263491ba88f03270b592
yveshield <yveshield@gmail.com>
2020-03-20 23:31:10 +0800

    17. Letter Combinations of a Phone Number

---
+++ 17.go
@@ -0,0 +1,34 @@
+package leetcode
+
+func letterCombinations(digits string) []string {
+	letters := make(map[byte][]string)
+	letters['2'] = []string{"a", "b", "c"}
+	letters['3'] = []string{"d", "e", "f"}
+	letters['4'] = []string{"g", "h", "i"}
+	letters['5'] = []string{"j", "k", "l"}
+	letters['6'] = []string{"m", "n", "o"}
+	letters['7'] = []string{"p", "q", "r", "s"}
+	letters['8'] = []string{"t", "u", "v"}
+	letters['9'] = []string{"w", "x", "y", "z"}
+
+	res := []string{}
+
+	if digits == "" {
+		return res
+	}
+
+	res = append(res, "")
+
+	for i := 0; i < len(digits); i++ {
+		nums := letters[digits[i]]
+		pre := res
+		res = []string{}
+		for _, num := range nums {
+			for _, v := range pre {
+				res = append(res, v+num)
+			}
+		}
+	}
+
+	return res
+}
