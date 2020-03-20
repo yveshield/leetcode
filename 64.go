723ff22c94aaa837cd70aee5bf003b0d7f2cf7c3
yveshield <yveshield@gmail.com>
2020-03-21 00:38:21 +0800

    64. Minimum Path Sum

---
+++ 64.go
@@ -0,0 +1,25 @@
+package leetcode
+
+func minPathSum(grid [][]int) int {
+	row := len(grid)
+	col := len(grid[0])
+	for i := 0; i < row; i++ {
+		for j := 0; j < col; j++ {
+			if i == 0 && j == 0 {
+				continue
+			}
+			if i == 0 {
+				grid[i][j] += grid[i][j-1]
+			} else if j == 0 {
+				grid[i][j] += grid[i-1][j]
+			} else {
+				if grid[i][j-1] < grid[i-1][j] {
+					grid[i][j] += grid[i][j-1]
+				} else {
+					grid[i][j] += grid[i-1][j]
+				}
+			}
+		}
+	}
+	return grid[row-1][col-1]
+}
