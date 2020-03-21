bdefd12333e23eb8fdd693669261ea97dcd6584e
yveshield <yveshield@gmail.com>
2020-03-21 19:35:53 +0800

    287. Find the Duplicate Number, O(1) extra space

--- 287.go
+++ 287.go
@@ -1,12 +1,21 @@
 package main
 
 func findDuplicate(nums []int) int {
-	dp := make(map[int]int)
-	for i := 0; i < len(nums); i++ {
-		if _, ok := dp[nums[i]]; ok {
-			return nums[i]
+	left := 1
+	right := len(nums) - 1
+	for left < right {
+		mid := (left + right) >> 1
+		cnt := 0
+		for i := 0; i < len(nums); i++ {
+			if nums[i] <= mid {
+				cnt++
+			}
 		}
-		dp[nums[i]] = 1
+		if cnt > mid {
+			right = mid
+		} else {
+			left = mid + 1
+		}
 	}
-	return 0
+	return left
 }
