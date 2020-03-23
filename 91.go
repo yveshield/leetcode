f937cb599cbc0d08341f1caf43cc82966f6f573d
yveshield <yveshield@gmail.com>
2020-03-24 00:17:54 +0800

    91. Decode Ways

---
+++ 91.go
@@ -0,0 +1,35 @@
+package main
+
+func numDecodings(s string) int {
+	s0 := int('0')
+	dp := make([]int, len(s)+1)
+	dp[0] = 1
+	dp[1] = 1
+	if s[0] == '0' {
+		dp[1] = 0
+	}
+	if len(s) <= 1 {
+		return dp[1]
+	}
+	for i := 2; i <= len(s); i++ {
+		a := int(s[i-2]) - s0
+		b := int(s[i-1]) - s0
+		n := a*10 + b
+		// 至少一个0独处，无法解码
+		if s[i-1] == '0' && s[i-2] == '0' {
+			return 0
+		} else if s[i-2] == '0' { // i-1必需独立，增加i位时，组合数不变
+			dp[i] = dp[i-1]
+		} else if s[i-1] == '0' {
+			if n > 26 {
+				return 0
+			}
+			dp[i] = dp[i-2] //abc10+x，10和x必需独立
+		} else if n > 26 { //abc34+x
+			dp[i] = dp[i-1]
+		} else { //abc23+x
+			dp[i] = dp[i-1] + dp[i-2]
+		}
+	}
+	return dp[len(s)]
+}
