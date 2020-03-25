2e7cbf1e963ae6132d3634e3f1bc8e6215cb130b
yveshield <yveshield@gmail.com>
2020-03-25 17:32:29 +0800

    105. Construct Binary Tree from Preorder and Inorder Traversal

---
+++ 105.go
@@ -0,0 +1,24 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func buildTree(preorder []int, inorder []int) *TreeNode {
+	if len(preorder) == 0 {
+		return nil
+	}
+	i := 0
+	for ; i < len(inorder); i++ {
+		if inorder[i] == preorder[0] {
+			break
+		}
+	}
+	root := &TreeNode{}
+	root.Val = preorder[0]
+	root.Left = buildTree(preorder[1:i+1], inorder[0:i])
+	root.Right = buildTree(preorder[i+1:], inorder[i+1:])
+	return root
+}
