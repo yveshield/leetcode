69e189015fa745784a0dba85746042b8f63357e7
yveshield <yveshield@gmail.com>
2021-02-17 10:46:21 +0800

    84

--- 84.go
+++ 84.go
@@ -2,26 +2,37 @@ package main
 
 func largestRectangleArea(heights []int) int {
 	l := len(heights)
-	max := 0
+	if l == 0 {
+		return 0
+	}
+	lessFromLeft := make([]int, l)
+	lessFromRight := make([]int, l)
+	lessFromRight[l-1] = l
+	lessFromLeft[0] = -1
+	for i := 1; i < l; i++ {
+		p := i - 1
+
+		for p >= 0 && heights[p] >= heights[i] {
+			p = lessFromLeft[p]
+		}
+		lessFromLeft[i] = p
+	}
+
+	for i := l - 2; i >= 0; i-- {
+		p := i + 1
+
+		for p < l && heights[p] >= heights[i] {
+			p = lessFromRight[p]
+		}
+		lessFromRight[i] = p
+	}
+
+	maxArea := 0
 	for i := 0; i < l; i++ {
-		area := 0
-		for j := i; j >= 0; j-- {
-			if heights[j] >= heights[i] {
-				area += heights[i]
-			} else {
-				break
-			}
-		}
-		for j := i + 1; j < l; j++ {
-			if heights[j] >= heights[i] {
-				area += heights[i]
-			} else {
-				break
-			}
-		}
-		if area > max {
-			max = area
+		max := heights[i] * (lessFromRight[i] - lessFromLeft[i] - 1)
+		if max > maxArea {
+			maxArea = max
 		}
 	}
-	return max
+	return maxArea
 }
